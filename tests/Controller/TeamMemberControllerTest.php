062b7d563f82cd16d70b3b97ec25e43facb38afb
izemaghilas <saidtala23@gmail.com>
2022-03-08 15:40:55 +0100

    test edit team member

--- tests/Controller/TeamMemberControllerTest.php
+++ tests/Controller/TeamMemberControllerTest.php
@@ -197,4 +197,66 @@ class TeamMemberControllerTest extends WebTestCase
         $this->assertSame('ameksaafulay@gmail.com', $secondTeamMember->eq(2)->text());
         $this->assertSame('2010-01-05', $secondTeamMember->eq(3)->text());
     }
+
+    public function testEditTeamMemberSuccessful(): void
+    {
+        $firstName = $this->faker->firstName();
+        $lastName = $this->faker->lastName();
+        $email = $this->faker->email();
+        $birthDate = $this->faker->date();
+
+        $this->client->request('GET', self::TEAM_MEMBERS_URL);
+        $crawler = $this->client->clickLink('edit');
+        $form = $crawler->selectButton('Edit')->form();
+        
+        $this->client->submitForm('Edit', [
+            'team_member[firstName]' => $firstName,
+            'team_member[lastName]' => $lastName,
+            'team_member[email]' => $email,
+            'team_member[birthDate]' => $birthDate
+        ]);
+
+        $crawler = $this->client->followRedirect();
+        $teamMembers = $crawler->filter('body > div')->children();
+        $firstTeamMember = $teamMembers->eq(0)->children();
+        $secondTeamMember = $teamMembers->eq(1)->children();
+
+        $this->assertSame('Aghilas', $form['team_member[firstName]']->getValue());
+        $this->assertSame('IZEM', $form['team_member[lastName]']->getValue());
+        $this->assertSame('izemaghilas@gmail.com', $form['team_member[email]']->getValue());
+        $this->assertSame('2000-10-25', $form['team_member[birthDate]']->getValue());
+
+        $this->assertCount(2, $teamMembers);
+        $this->assertSame($firstName, $firstTeamMember->eq(0)->text());
+        $this->assertSame($lastName, $firstTeamMember->eq(1)->text());
+        $this->assertSame($email, $firstTeamMember->eq(2)->text());
+        $this->assertSame($birthDate, $firstTeamMember->eq(3)->text());
+        
+        $this->assertSame('Afulay', $secondTeamMember->eq(0)->text());
+        $this->assertSame('AMEKSA', $secondTeamMember->eq(1)->text());
+        $this->assertSame('ameksaafulay@gmail.com', $secondTeamMember->eq(2)->text());
+        $this->assertSame('2010-01-05', $secondTeamMember->eq(3)->text());
+    }
+
+    public function testEditTeamMemberNotOnDataBase(): void
+    {
+        $this->client->request('POST', self::TEAM_MEMBERS_URL.'/12/edit');
+        
+        $crawler = $this->client->followRedirect();
+        $teamMembers = $crawler->filter('body > div')->children();
+        $firstTeamMember = $teamMembers->eq(0)->children();
+        $secondTeamMember = $teamMembers->eq(1)->children();
+
+        $this->assertCount(2, $teamMembers);
+        
+        $this->assertSame('Aghilas', $firstTeamMember->eq(0)->text());
+        $this->assertSame('IZEM', $firstTeamMember->eq(1)->text());
+        $this->assertSame('izemaghilas@gmail.com', $firstTeamMember->eq(2)->text());
+        $this->assertSame('2000-10-25', $firstTeamMember->eq(3)->text());
+        
+        $this->assertSame('Afulay', $secondTeamMember->eq(0)->text());
+        $this->assertSame('AMEKSA', $secondTeamMember->eq(1)->text());
+        $this->assertSame('ameksaafulay@gmail.com', $secondTeamMember->eq(2)->text());
+        $this->assertSame('2010-01-05', $secondTeamMember->eq(3)->text());
+    }
 }
