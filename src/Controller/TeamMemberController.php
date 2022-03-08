f9e2a90bd50c6542bd314d119d74874cf02a4e80
izemaghilas <saidtala23@gmail.com>
2022-03-08 15:41:22 +0100

    edit team member

--- src/Controller/TeamMemberController.php
+++ src/Controller/TeamMemberController.php
@@ -24,11 +24,18 @@ class TeamMemberController extends AbstractController
     #[Route('/team-members/hire', name: 'team_members_hire', methods: ['GET', 'POST'])]
     public function hire(Request $request, ManagerRegistry $doctrine): Response
     {
-        $form = $this->createForm(TeamMemberType::class, new TeamMember());
+        $form = $this->createForm(
+            TeamMemberType::class, 
+            new TeamMember(), 
+            [
+                'submit_button_label' => 'Hire'
+            ]
+        );
         
         $form->handleRequest($request);
         
-        if($form->isSubmitted() && $form->isValid()){
+        if($form->isSubmitted() && $form->isValid())
+        {
             $entityManager = $doctrine->getManager();
             $teamMember = $form->getData();
             $entityManager->persist($teamMember);
@@ -42,12 +49,43 @@ class TeamMemberController extends AbstractController
         ]);
     }
 
+    #[Route('/team-members/{id}/edit', name: 'team_members_edit', methods: ['GET', 'POST'])]
+    public function edit(int $id, Request $request, ManagerRegistry $doctrine): Response
+    {
+        $teamMember = $doctrine->getRepository(TeamMember::class)->find($id);
+        if(!isset($teamMember)){
+            return $this->redirectToRoute('team_members');
+        }
+
+        $form = $this->createForm(
+            TeamMemberType::class, 
+            $teamMember, 
+            [
+                'submit_button_label' => 'Edit'
+            ]
+        );
+        
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $entityManager = $doctrine->getManager();
+            $entityManager->flush();
+            return $this->redirectToRoute('team_members');
+        }
+        
+        return $this->renderForm('team-members/edit.html.twig', [
+            'form' => $form
+        ]);
+    }
+
     #[Route('/team-members/{id}/delete', name: 'team_members_delete', methods: ['GET'])]
     public function delete(int $id, ManagerRegistry $doctrine): Response
     {   
         $entityManager = $doctrine->getManager();
         $teamMember = $doctrine->getRepository(TeamMember::class)->find($id);
-        if(isset($teamMember)){
+        if(isset($teamMember))
+        {
             $entityManager->remove($teamMember);
             $entityManager->flush();
         }
